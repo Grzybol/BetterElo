24219db2dc99978ab6dd1919cdf468c2ebd32620
Grzybol <grzybol98@gmail.com>
2023-11-21 14:33:20 +0100

    - changed broadcastFormattedChatMessage

--- src/main/java/betterbox/mine/game/betterelo/ChatNotifier.java
+++ src/main/java/betterbox/mine/game/betterelo/ChatNotifier.java
@@ -1,7 +1,10 @@
 package betterbox.mine.game.betterelo;
 
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.Bukkit;
 
 public class ChatNotifier extends BukkitRunnable {
 
@@ -14,9 +17,26 @@ public class ChatNotifier extends BukkitRunnable {
     @Override
     public void run() {
         // Wiadomość, którą chcesz wysłać
-        String message = ChatColor.GOLD + "" + ChatColor.BOLD + "[BetterElo]" + ChatColor.AQUA + " Remember to use /be claim to claim your rewards!";
+        String message = " Remember to use /be claim to claim your rewards!";
+        String prefix = "[BetterElo]";
+        broadcastFormattedChatMessage(prefix, message);
+        message = " Use /shop  to get our Item-Shop link";
+        broadcastFormattedChatMessage(prefix, message);
+        message = " Use /discord  to get our Discord link";
+        broadcastFormattedChatMessage(prefix, message);
+    }
+    public void broadcastFormattedChatMessage(String prefix, String message) {
+        Component componentPart1 = Component.text(prefix)
+                .color(NamedTextColor.GOLD) // Ustawienie pierwszego koloru
+                .decorate(TextDecoration.BOLD); // Pierwsze formatowanie
 
-        // Wysyłanie wiadomości na czat
-        plugin.getServer().broadcastMessage(message);
+        Component componentPart2 = Component.text(message)
+                .color(NamedTextColor.AQUA); // Ustawienie drugiego koloru
+
+        // Łączenie obu komponentów w jeden
+        Component finalComponent = componentPart1.append(componentPart2);
+
+        // Wysłanie komponentu do wszystkich graczy na serwerze
+        Bukkit.getServer().broadcast(finalComponent);;
     }
 }
