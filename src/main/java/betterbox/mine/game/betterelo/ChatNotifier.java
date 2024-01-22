4b1e5680a7c110d86ff6d64c087a62d616c62490
Grzybol <grzybol98@gmail.com>
2024-01-22 23:32:15 +0100

    - fixed creating config.yml file

--- src/main/java/betterbox/mine/game/betterelo/ChatNotifier.java
+++ src/main/java/betterbox/mine/game/betterelo/ChatNotifier.java
@@ -1,8 +1,5 @@
 package betterbox.mine.game.betterelo;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.Bukkit;
 
@@ -19,24 +16,15 @@ public class ChatNotifier extends BukkitRunnable {
         // Wiadomość, którą chcesz wysłać
         String message = " Remember to use /be claim to claim your rewards!";
         String prefix = "[BetterElo]";
-        broadcastFormattedChatMessage(prefix, message);
+        broadcastFormattedChatMessage(prefix + message);
         message = " Use /shop  to get our Item-Shop link";
-        broadcastFormattedChatMessage(prefix, message);
+        broadcastFormattedChatMessage(prefix + message);
         message = " Use /discord  to get our Discord link";
-        broadcastFormattedChatMessage(prefix, message);
+        broadcastFormattedChatMessage(prefix + message);
     }
-    public void broadcastFormattedChatMessage(String prefix, String message) {
-        Component componentPart1 = Component.text(prefix)
-                .color(NamedTextColor.GOLD) // Ustawienie pierwszego koloru
-                .decorate(TextDecoration.BOLD); // Pierwsze formatowanie
 
-        Component componentPart2 = Component.text(message)
-                .color(NamedTextColor.AQUA); // Ustawienie drugiego koloru
-
-        // Łączenie obu komponentów w jeden
-        Component finalComponent = componentPart1.append(componentPart2);
-
-        // Wysłanie komponentu do wszystkich graczy na serwerze
-        Bukkit.getServer().broadcast(finalComponent);;
+    public void broadcastFormattedChatMessage(String message) {
+        // Wysłanie wiadomości do wszystkich graczy na serwerze
+        Bukkit.getServer().broadcastMessage(message);
     }
 }
