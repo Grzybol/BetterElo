8de9bd4530ffcba3efc2cbffdd79b6f314cc3e54
Grzybol <grzybol98@gmail.com>
2024-04-05 18:35:22 +0200

    - fixes...

--- src/main/java/betterbox/mine/game/betterelo/Event.java
+++ src/main/java/betterbox/mine/game/betterelo/Event.java
@@ -636,12 +636,19 @@ public class  Event implements Listener {
         return false;
     }
     public boolean hasZephyrLore(Player player) {
+        pluginLogger.log(PluginLogger.LogLevel.ZEPHYR,"Event.hasZephyrLore called, player "+player.getName());
         ItemStack itemStack = player.getInventory().getItemInMainHand();
+        pluginLogger.log(PluginLogger.LogLevel.ZEPHYR,"Event.hasZephyrLore itemInHand: "+itemStack.displayName());
         ItemStack chestplate = player.getInventory().getChestplate();
-        if (chestplate.getType().toString().contains("ELYTRA") || hasElytraLore(chestplate)) {
-            pluginLogger.log(PluginLogger.LogLevel.ZEPHYR,"Event.hasZephyrLore player "+player.getName()+" is wearing Elytra! Aborting");
-            player.sendMessage((ChatColor.GOLD + "" + ChatColor.BOLD + "[BetterElo] " + ChatColor.DARK_RED + "You cannot use Zephyr while wearing Elytra!"));
-            return false;
+        try{
+            pluginLogger.log(PluginLogger.LogLevel.ZEPHYR, "Event.hasZephyrLore chestplate: " + chestplate.displayName());
+            if (chestplate.getType().toString().contains("ELYTRA") || hasElytraLore(chestplate)) {
+                pluginLogger.log(PluginLogger.LogLevel.ZEPHYR, "Event.hasZephyrLore player " + player.getName() + " is wearing Elytra! Aborting");
+                player.sendMessage((ChatColor.GOLD + "" + ChatColor.BOLD + "[BetterElo] " + ChatColor.DARK_RED + "You cannot use Zephyr while wearing Elytra!"));
+                return false;
+            }
+        }catch(Exception e){
+            pluginLogger.log(PluginLogger.LogLevel.ERROR, "Event.hasZephyrLore exception:  " +e.getMessage());
         }
 
 
